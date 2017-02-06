0896917ae9e40ed9237eb8ce782c8fbe3b9a1bed
Mousecop <joaskew13@gmail.com>
2017-02-06 14:12:37 -0500

    fixed state object

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-var quizState = [
-  {
-    question: "Who plays Barney Stinson?",
-    choices: ["Neil Patrick Harris", "Jared Leto", "John Travolta","Jason Seigel"],
-    correctAnswer: 0
-  },
-  {
-    question: "Where did Lilly and Marshall get married?",
-    choices: ["On the beach", "The White House", "Van Smoote House", "In Atlantic City"],
-    correctAnswer:  2
-  },
-  {
-    question: "What is the name of the bar the group goes to all the time?",
-    choices: ["Frosty's", "McGees", "Hand-in-Hand", "MacLarens"],
-    correctAnswer: 3
-  },
-  {
-    question: "Who does Robin get married to?",
-    choices: ["Marshall", "Ted", "Barney", "Lilly"],
-    correctAnswer: 2
-  },
-  {
-    question: "What is Ted Mosby's job?",
-    choices: ["Sailor", "Architect", "Engineer", "Software Developer"],
-    correctAnswer: 1
-  }
-];
+var quizState = {
+  questions: [
+    {
+      questionText: "Who plays Barney Stinson?",
+      choices: ["Neil Patrick Harris", "Jared Leto", "John Travolta","Jason Seigel"],
+      correctAnswer: 0
+    },
+    {
+      questionText: "Where did Lilly and Marshall get married?",
+      choices: ["On the beach", "The White House", "Van Smoote House", "In Atlantic City"],
+      correctAnswer:  2
+    },
+    {
+      questionText: "What is the name of the bar the group goes to all the time?",
+      choices: ["Frosty's", "McGees", "Hand-in-Hand", "MacLarens"],
+      correctAnswer: 3
+    },
+    {
+      questionText: "Who does Robin get married to?",
+      choices: ["Marshall", "Ted", "Barney", "Lilly"],
+      correctAnswer: 2
+    },
+    {
+      questionText: "What is Ted Mosby's job?",
+      choices: ["Sailor", "Architect", "Engineer", "Software Developer"],
+      correctAnswer: 1
+    }
+  ]
+};
 
 
-function makeQuestions(index) {
-  var qElement = $('div', {id: questions});
-}
+console.log(quizState.questions[0]);
