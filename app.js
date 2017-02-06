5f929105beaab706a9ef54653473ed9aba341129
Mousecop <joaskew13@gmail.com>
2017-02-06 12:48:58 -0500

    Start of Quiz app. Started structure of code

---
+++ app.js
@@ -0,0 +1,27 @@
+var quizState = [
+  {
+    question: "Who plays Barney Stinson?",
+    choices: ["Neil Patrick Harris", "Jared Leto", "John Travolta","Jason Seigel"],
+    correctAnswer: "Neil Patrick Harris"
+  },
+  {
+    question: "Where did Lilly and Marshall get married?",
+    choices: ["On the beach", "The White House", "Van Smoote House", "In Atlantic City"],
+    correctAnswer: "Van Smoote House"
+  },
+  {
+    question: "What is the name of the bar the group goes to all the time?",
+    choices: ["Frosty's", "McGees", "Hand-in-Hand", "MacLarens"],
+    correctAnswer: "MacLarens"
+  },
+  {
+    question: "Who does Robin get married to?",
+    choices: ["Marshall", "Ted", "Barney", "Lilly"],
+    correctAnswer: "Barney"
+  },
+  {
+    question: "What is Ted Mosby's job?",
+    choices: ["Sailor", "Architect", "Engineer", "Software Developer"],
+    correctAnswer: "Architect"
+  }
+];
