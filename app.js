6d0e7319bb95144eac8f318411932b9256a05223
Mousecop <joaskew13@gmail.com>
2017-02-06 16:27:10 -0500

    made state functions for checking answers

--- app.js
+++ app.js
@@ -31,11 +31,61 @@ var quizState = {
   ]
 };
 
+function welcomeScreen(state, element) {
+  var welcomeHtml =  '<h1>Quiz App</h1>' +
+          '<p>How well do you know How I Met Your Mother?</p>' +
+          '<button type="button" class="welcomeButton">Start Here</button>';
+  element.html(welcomeHtml);
+}
+
 function submitAnswer(state, index) {
   state.userAnswers.push(index);
+  console.log(quizState.userAnswers);
 }
 
 function startQuiz(state) {
   state.score = 0;
   state.currentQuestion = 0;
 }
+
+
+function checkAnswer(state, index) {
+  if (state.userAnswers[state.userAnswers.length - 1] == state.questions[index].correctAnswer) {
+    displayCorrectAnswer(state, $('.answers'));
+  } else {
+    displayWrongAnswer(state, $('.answers'));
+  }
+}
+function displayCorrectAnswer(state, element) {
+  var answerHtml = "<p>That was Legend....wait for it.... dary. LEGENDARY!</p>"
+  element.html(answerHtml);
+}
+function displayWrongAnswer(state, element) {
+  var answerHtml = "<p>You are not worthy</p>";
+  element.html(answerHtml);
+}
+
+
+function renderQuestions(state, index) {
+  
+}
+
+
+
+
+
+
+
+
+$(function eventListeners() {
+  welcomeScreen(quizState, $('.quiz'));
+  $('.quiz').on('click', '.welcomeButton', function(event) {
+
+  });
+});
+
+
+
+
+// submitAnswer(quizState, 0);
+// checkAnswer(quizState, 0);
