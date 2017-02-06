cd7f74400fbd93a3041c9792eb2bc847c6cf5564
Mousecop <joaskew13@gmail.com>
2017-02-06 13:38:47 -0500

    changed correctAnswer key/value

--- app.js
+++ app.js
@@ -2,26 +2,31 @@ var quizState = [
   {
     question: "Who plays Barney Stinson?",
     choices: ["Neil Patrick Harris", "Jared Leto", "John Travolta","Jason Seigel"],
-    correctAnswer: "Neil Patrick Harris"
+    correctAnswer: 0
   },
   {
     question: "Where did Lilly and Marshall get married?",
     choices: ["On the beach", "The White House", "Van Smoote House", "In Atlantic City"],
-    correctAnswer: "Van Smoote House"
+    correctAnswer:  2
   },
   {
     question: "What is the name of the bar the group goes to all the time?",
     choices: ["Frosty's", "McGees", "Hand-in-Hand", "MacLarens"],
-    correctAnswer: "MacLarens"
+    correctAnswer: 3
   },
   {
     question: "Who does Robin get married to?",
     choices: ["Marshall", "Ted", "Barney", "Lilly"],
-    correctAnswer: "Barney"
+    correctAnswer: 2
   },
   {
     question: "What is Ted Mosby's job?",
     choices: ["Sailor", "Architect", "Engineer", "Software Developer"],
-    correctAnswer: "Architect"
+    correctAnswer: 1
   }
 ];
+
+
+function makeQuestions(index) {
+  var qElement = $('div', {id: questions});
+}
